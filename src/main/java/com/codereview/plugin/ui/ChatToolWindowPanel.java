dc87a3ee1ab3af99b2c80dfeda8a0df05c87668f
liwanli <402947715@qq.com>
2025-07-28 13:12:35 +0800

    修改面板提示语

--- src/main/java/com/codereview/plugin/ui/ChatToolWindowPanel.java
+++ src/main/java/com/codereview/plugin/ui/ChatToolWindowPanel.java
@@ -333,7 +333,7 @@ public class ChatToolWindowPanel extends JBPanel<ChatToolWindowPanel> {
         JButton bigLoginButton = (JButton) welcomePanel.getClientProperty("bigLoginButton");
 
         if (isLoggedIn) {
-            descLabel.setText("<html>🎉 欢迎使用鼎码智辅！我是你的AI编码助手<br/>" +
+            descLabel.setText("<html>我是你的AI编码助手，欢迎使用鼎码智辅！🎉 <br/>" +
 //                    "可以帮你快速生成校验器代码<br/>" +
 //                    "💡 使用方法很简单：<br/>" +
 //                    "在输入框中输入API名称或校验器名称即可<br/>" +
